f2f295f01f256c9dfe9d0b245509d448978dc447
Holyson <holysonmand304@gmail.com>
2026-02-02 00:42:43 +0100

    Ajout des vues et du formulaire

--- recettes/admin.py
+++ recettes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import recette
 
 # Register your models here.
+@admin.register(recette)
+class RecetteAdmin(admin.ModelAdmin):
+    list_display = ('date', 'montant', 'description')
--- recettes/views.py
+++ recettes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from .models import Recette
+from .forms import RecetteForm
+
+def liste_recettes(request):
+    recettes = Recette.objects.all()
+    return render(request, 'recettes/liste.html', {'recettes': recettes})
+
+def ajouter_recette(request):
+    form = RecetteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('liste_recettes')
+    return render(request, 'recettes/ajouter.html', {'form': form})
