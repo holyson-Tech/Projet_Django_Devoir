e7e31c15ed84cecfe241ab10b7238042ad914fda
Holyson <holysonmand304@gmail.com>
2026-02-02 01:14:29 +0100

    Essais de test 1

--- recettes/urls.py
+++ recettes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import liste_recettes, ajouter_recette
 
+
 urlpatterns = [
     path('', liste_recettes, name='liste_recettes'),
     path('ajouter/', ajouter_recette, name='ajouter_recette'),
